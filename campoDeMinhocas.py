73f78a43842723beec543324f751a450585d48b6
Sidney Felintro <sidneyfelintro65@gmail.com>
2026-02-25 23:46:23 -0300

    adicionando desafio campo de minhocas.

---
+++ campoDeMinhocas.py
@@ -0,0 +1,15 @@
+linhaseColunas = []
+SomaDosValores = []
+
+tamMatriz = list(map(int, input().split()))
+
+for i in range(tamMatriz[0]):
+    linhaseColunas.append(list(map(int, input().split())))
+
+for c in linhaseColunas:
+    SomaDosValores.append(sum(c))
+
+for valores in zip(*linhaseColunas):
+    SomaDosValores.append(sum(valores))
+
+print(max(SomaDosValores))
